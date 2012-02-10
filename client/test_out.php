27f0a9ec031e9320b39e55f288fba4c8585c53f7
rlbisbe <rober90@gmail.com>
2012-02-10 20:27:34 +0100

    Renamed test-out

--- client/test_out.php
+++ client/test_out.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 include_once 'APICaller.php';
 
-$apicaller = new ApiCaller('APP001','28e336ac6c9423d946ba02d19c6a2632','http://localhost:61148/index.php');
+$apicaller = new ApiCaller('APP001','28e336ac6c9423d946ba02d19c6a2632','http://gentle-mist-3984.herokuapp.com/index.php');
 
 $todo_items = $apicaller->sendRequest(array(
     'controller' => 'Lab',
